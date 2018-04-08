83d7211aabd27eded58469ce59ffd5988b90d113
MIN-PC\min <minjr@lianchuang.com>
2018-04-08 16:25:13 +0800

    热部署及json

--- src/main/java/com/min/training/controller/HelloController.java
+++ src/main/java/com/min/training/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.min.training.controller;
 
+import com.min.training.model.Dog;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +13,12 @@ public class HelloController {
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String Hello(){
         return String.valueOf(new Random().nextInt(1000));
+    }
+    @RequestMapping(value = "/respJson",method = RequestMethod.GET)
+    public Dog respJson(){
+        Dog dog = new Dog();
+        dog.setAge(12);
+        dog.setName("萨122摩耶2");
+        return dog;
     }
 }
